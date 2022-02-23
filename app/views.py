9c040a147da2f1e58c39425d87d504948ce887e4
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-23 13:36:06 +0300

    add control flow in the index file

--- app/views.py
+++ app/views.py
@@ -11,10 +11,10 @@ def index():
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+# @app.route('/movie/<int:movie_id>')
+# def movie(movie_id):
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html',id = movie_id)
+#     '''
+#     View movie page function that returns the movie details page and its data
+#     '''
+#     return render_template('movie.html',id = movie_id)
