935ac88006301d12a2f955fa935168b663a2878e
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-24 18:30:20 +0300

    create an error page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from .request import get_movies
+from .request import get_movies,get_movie
 
 # Views
 @app.route('/')
@@ -16,10 +17,12 @@ def index():
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-# @app.route('/movie/<int:movie_id>')
-# def movie(movie_id):
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
 
-#     '''
-#     View movie page function that returns the movie details page and its data
-#     '''
-#     return render_template('movie.html',id = movie_id)
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    return render_template('movie.html',title=title , movie=movie)
